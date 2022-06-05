f3647d3a3987f68284d99c20dfe97276513a90bf
Nick DeSanctis <njdesanctis@gmail.com>
2022-06-05 14:09:33 -0400

    added some more files

---
+++ code_hash.py
@@ -0,0 +1,16 @@
+
+# initial idea: create a hash that goes to language
+# each language then relates to its syntax
+
+code_hash = {
+    ".py" : {
+        "Int" : "",
+        "Float" : "",
+        "String" : ""
+    },
+    ".jl" : {
+        "Int" : "",
+        "Float" : "",
+        "String" : ""
+    }
+}
---
+++ syntax_tree.ml
@@ -0,0 +1,7 @@
+(* Create abstract syntax tree for code *)
+
+type basic =
+  | Int of int
+  | Float of float
+  | String of string
+
